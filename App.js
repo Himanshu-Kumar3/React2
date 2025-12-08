e34cdc87283d23478ae6a948e83342b39f98be95
Himanshu Kumar <himansu9508@gmail.com>
2025-12-08 22:10:21 +0530

    created a create react

--- App.js
+++ App.js
@@ -1,35 +1,18 @@
-const h1 = React.createElement('h1' , {id:'h1' , xyz:'xyz'} , 'Hello with react');
-console.log(h1);  // returns object
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(h1);
-
-
-
-// For nested 
-/* <div>
-     <div id="child1"> 
-        <h1> </h1>
-        <h2> </h2>
-     </div> 
-     <div id="child2"> 
-        <h1> </h1>
-        <h2> </h2>
-     </div>  
-   </div>
-*/
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // For multiple childs i.e siblings of the child we have to create array of the child 
 
 
 // This looks more messy than the actual html :- so for this we use jsx not js
 const parent = React.createElement('div' ,{id:'parent'} ,
-    [React.createElement('div' ,{id:'child'} ,
-      [ React.createElement('h1' ,{} , 'H1 from nested div') ,
-      React.createElement('h2' , {} ,'Hi I am  H2 ') ]) ,
-   React.createElement('div' ,{id:'child2'} ,
-      [ React.createElement('h1' ,{} , 'H1 from nested div') ,
-      React.createElement('h2' , {} ,'Hi I am  H2 ') ]) ] );
-root.render(parent)
+    [React.createElement('div' ,{id:"child" , key:"child1"} ,
+      [ React.createElement('h1' ,{key:"h1-1"} , 'H1 from nested div') ,
+      React.createElement('h2' , {key:"h2-1"} ,'Hi I am  H2 ') ]) ,
+
+   React.createElement('div' ,{id:"child2" , key:"child2"} ,
+      [ React.createElement('h1' ,{key:"h1-2"} , 'H1 from nested div') ,
+      React.createElement('h2' , {key:"h2-2"} ,'Hi I am  H2 ') ]) ] );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  
+root.render(parent);
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # React 
 this is a repo for react class
 
+# parcel
+- Dev Build
+- Local server
+- HMR - Hot Module Replacement
+- File watching algorithm :- Written in C++
+- Caching :- Build faster 
+- Image Optimization
+- Minification
+- Bundle
+- Compress our file
+- Consistent hashing
+- Code splitting
+- Differential building :- Supports older browser
+- Diagnostic
+- Error handling 
+- Can host proj. HTTPs
+- Tree shaking :- Remove unused code
+- Different dev and prod bundles
+
