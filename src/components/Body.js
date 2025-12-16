a7797fa5dfa2530ccdc226b84cae63b50c47900e
Himanshu Kumar <himansu9508@gmail.com>
2025-12-16 22:40:15 +0530

    added responsive data , search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,73 @@
 import ResCard from "../components/ResCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 // Body :-
    // not using keys(Not acceptable) <<<<< index as key <<<<<<<<<<<<<<<<<<<<<<< unique id (Best practice :) )
 const Body = ()=>{
     // Local useState Variable = superPowerFul state variable : for this we use : "useSatate() :- Hook"
-    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant , setFilteredRestaurant] = useState([]); // copy of original // for changes to be done in original data
 
 
-   return(
+    const [searchText , setSearchText] = useState("")
+
+
+   //  useEffect():-
+   useEffect(()=>{
+      console.log("Use Effect");
+      fetchData();
+   }, []);
+
+
+   const fetchData= async ()=>{
+           const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.71700&lng=75.83370&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+           const json = await data.json();
+           console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+           setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+           setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+   }
+
+
+ 
+   return listOfRestaurant.length === 0? (<Shimmer/>): (
       <div className="body">
-            <button className="filter-btn" onClick ={()=>{
-                const filterList = listOfRestaurant.filter((rest)=> rest.info.avgRating > 4);
-                setListOfRestaurant(filterList);
-            }} >Top Rated Restaurant</button>
-            <div className="res-card-container">
-               {/* everytime it iterate into the list element -> obj , pass that obj to the ResCard component as props -> argument and also key */}
-                {listOfRestaurant.map((restaurant) =>(
-                  <ResCard key={restaurant.info.id} resData = {restaurant} />
-                ))}
 
-               {/* <ResCard resName="KFC" cusine="Burger, French Fries , Chicken Fires" imgLink="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/11/4df6c301-66e9-4b76-83cf-c44dfe08eaf7_975195.JPG"/> */}
-            </div>
+
+         <div className="filter">
+            <div className="searchContainer">
+            <input 
+             type="text"  
+             className="search-box" 
+             value={searchText} 
+             onChange={(e)=>{
+               setSearchText(e.target.value);
+             }}>  
+             </input>
+            <button className="searchBtn" onClick={()=>{
+               const filteredList = listOfRestaurant.filter((res)=>{
+                  return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+                  }); 
+               console.log(filteredList);
+               setFilteredRestaurant(filteredList);
+            }}> Search</button>
          </div>
+
+         <button className="filter-btn" onClick ={()=>{
+             const filterList = listOfRestaurant.filter((rest)=> rest.info.avgRating > 4);
+             setFilteredRestaurant(filterList);
+            }} >Top Rated Restaurant</button>
+         </div>
+
+         <div className="res-card-container">
+            {/* everytime it iterate into the list element -> obj , pass that obj to the ResCard component as props -> argument and also key */}
+            {filteredRestaurant.map((restaurant) =>(
+                  <ResCard key={restaurant.info.id} resData = {restaurant} />
+             ))}
+
+             {/* <ResCard resName="KFC" cusine="Burger, French Fries , Chicken Fires" imgLink="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/11/4df6c301-66e9-4b76-83cf-c44dfe08eaf7_975195.JPG"/> */}
+         </div>
+      </div>
    )
 };
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState } from "react";
 // Head :-
-const Header = ()=>
-     (
+const Header = ()=>{
+  const [ChangeloginBtn , setChangeLoginBtn] = useState("Login");
+
+    
+  return (    
       <div className="head">
             <div className="logo">
               <img className="logo-img" alt="logo-img"src={LOGO_URL}/>
@@ -12,9 +16,12 @@ const Header = ()=>
                <li>Contact us</li>
                <li>About</li>
                <li>Cart</li>
+               <button className="login-btn" onClick={()=>{
+                ChangeloginBtn== "Login" ? setChangeLoginBtn("Logout"):setChangeLoginBtn("Login")
+               }}>{ChangeloginBtn}</button>
              </ul>
             </div>
          </div>
    );
-
+  }
 export default Header;   
