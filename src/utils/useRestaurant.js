4adcb1ba1195512eae9be4a9d5896ff2dc19000c
Himanshu Kumar <himansu9508@gmail.com>
2025-12-27 21:24:51 +0530

    added custom res hook

---
+++ src/utils/useRestaurant.js
@@ -0,0 +1,24 @@
+import { useEffect  , useState} from "react";
+import { RESTAURANT_URL } from "./constant";
+
+
+const useRestaurant = ()=>{
+    const [restaurantInfo , setRestaurantInfo] = useState([]);
+
+    useEffect(()=>{
+        getRestaurantInfo();
+        },[]);
+   
+    const getRestaurantInfo = async()=>{
+  
+        const data = await fetch(RESTAURANT_URL);
+        const json = await data.json();
+        console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setRestaurantInfo(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+
+    return restaurantInfo ;
+}
+
+export default useRestaurant;
