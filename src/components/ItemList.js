aad618ca54467bf8867667a21e28d2816e89f266
Himanshu Kumar <himansu9508@gmail.com>
2025-12-28 18:24:59 +0530

    display Items

---
+++ src/components/ItemList.js
@@ -0,0 +1,48 @@
+import { ITEM_TYPE ,NESTED_ITEM  , CDN_URL} from "../utils/constant";
+import RestaurantCategory from "./RestaurantCategory";
+import {useState} from "react";
+import GetCardData from "./GetCardData";
+const ItemList = (props) =>{
+
+
+  const {itemData} = props;
+  const type = itemData?.["@type"];
+
+ const [showItemsById , setShowItemsById] = useState(null);
+ const handleClick = (categoryId)=>{
+          setShowItemsById(prev => prev === categoryId ? null : categoryId);
+          console.log(showItemsById);
+      }
+  if (type === ITEM_TYPE){
+    // console.log("Item cards",itemData?.itemCards);
+    return(
+        
+        <div>  
+            
+        <GetCardData data={itemData?.itemCards}/>
+        </div>
+    )} 
+    if (type === NESTED_ITEM){
+        // console.log();
+        return (
+            <div>
+                {itemData?.categories.map((category)=>{
+                    return(
+                        <div key={category.categoryId} className="text-left border-b border-gray-500 z-1">
+                            <div className="flex justify-between cursor-pointer py-2"  onClick={()=>handleClick(category.categoryId)}>
+                                <span className="font-bold py-2 border-b border-gray-400">{category.title} ({category?.itemCards.length})</span>
+                                <span>⬇️</span>
+                            </div>
+                             {showItemsById === category.categoryId && (<GetCardData  data={category?.itemCards}/>)}
+                        </div>
+                    )
+
+                })}
+              
+            </div>
+        )
+    }
+  
+
+}
+export default ItemList;
